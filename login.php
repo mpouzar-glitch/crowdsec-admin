073810f636213d576b1c9141a518485eee166a0f
mpouzar-glitch <m.pouzar@gmail.com>
2026-01-10 23:28:51 +0100

    change color background color

--- login.php
+++ login.php
@@ -33,7 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         body {
             font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, sans-serif;
-            background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
+           background: linear-gradient(135deg, #006666 0%, #33ff00 100%);
             min-height: 100vh;
             display: flex;
             align-items: center;
